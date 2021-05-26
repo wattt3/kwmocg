6a1df94a7eedb2f33f060af7db5362fe490f633c
ZenChu <81212761+ZenChu@users.noreply.github.com>
2021-05-26 18:01:58 +0900

    팀플

---
+++ teampro/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿
+namespace teampro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnSettings = new System.Windows.Forms.Button();
+            this.btnCOINS = new System.Windows.Forms.Button();
+            this.btnNEWS = new System.Windows.Forms.Button();
+            this.pnlMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.pnlMain.Controls.Add(this.button1);
+            this.pnlMain.Controls.Add(this.btnSettings);
+            this.pnlMain.Controls.Add(this.btnCOINS);
+            this.pnlMain.Controls.Add(this.btnNEWS);
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlMain.Location = new System.Drawing.Point(0, 0);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(800, 69);
+            this.pnlMain.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("배달의민족 주아", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(763, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSettings
+            // 
+            this.btnSettings.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnSettings.FlatAppearance.BorderSize = 0;
+            this.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSettings.Font = new System.Drawing.Font("배달의민족 주아", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSettings.ForeColor = System.Drawing.Color.White;
+            this.btnSettings.Image = global::teampro.Properties.Resources.Settings;
+            this.btnSettings.Location = new System.Drawing.Point(412, 0);
+            this.btnSettings.Name = "btnSettings";
+            this.btnSettings.Size = new System.Drawing.Size(257, 69);
+            this.btnSettings.TabIndex = 0;
+            this.btnSettings.Text = "Settings";
+            this.btnSettings.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSettings.UseVisualStyleBackColor = true;
+            this.btnSettings.Click += new System.EventHandler(this.btnSettings_Click);
+            this.btnSettings.Leave += new System.EventHandler(this.btnSettings_Leave_1);
+            // 
+            // btnCOINS
+            // 
+            this.btnCOINS.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnCOINS.FlatAppearance.BorderSize = 0;
+            this.btnCOINS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCOINS.Font = new System.Drawing.Font("배달의민족 주아", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnCOINS.ForeColor = System.Drawing.Color.White;
+            this.btnCOINS.Image = global::teampro.Properties.Resources.Coins;
+            this.btnCOINS.Location = new System.Drawing.Point(199, 0);
+            this.btnCOINS.Name = "btnCOINS";
+            this.btnCOINS.Size = new System.Drawing.Size(213, 69);
+            this.btnCOINS.TabIndex = 0;
+            this.btnCOINS.Text = "COINS";
+            this.btnCOINS.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCOINS.UseVisualStyleBackColor = true;
+            this.btnCOINS.Click += new System.EventHandler(this.btnCOINS_Click);
+            this.btnCOINS.Leave += new System.EventHandler(this.btnCOINS_Leave_1);
+            // 
+            // btnNEWS
+            // 
+            this.btnNEWS.Dock = System.Windows.Forms.DockStyle.Left;
+            this.btnNEWS.FlatAppearance.BorderSize = 0;
+            this.btnNEWS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNEWS.Font = new System.Drawing.Font("배달의민족 주아", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnNEWS.ForeColor = System.Drawing.Color.White;
+            this.btnNEWS.Image = global::teampro.Properties.Resources.Earth1;
+            this.btnNEWS.Location = new System.Drawing.Point(0, 0);
+            this.btnNEWS.Name = "btnNEWS";
+            this.btnNEWS.Size = new System.Drawing.Size(199, 69);
+            this.btnNEWS.TabIndex = 0;
+            this.btnNEWS.Text = "NEWS";
+            this.btnNEWS.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnNEWS.UseVisualStyleBackColor = true;
+            this.btnNEWS.Click += new System.EventHandler(this.btnNEWS_Click);
+            this.btnNEWS.Leave += new System.EventHandler(this.btnNEWS_Leave_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.CancelButton = this.button1;
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.pnlMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximumSize = new System.Drawing.Size(1600, 1000);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.Button btnSettings;
+        private System.Windows.Forms.Button btnCOINS;
+        private System.Windows.Forms.Button btnNEWS;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ teampro/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace teampro
+{
+    public partial class Form1 : Form
+    {
+        
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+
+        private static extern IntPtr CreateRoundRectRgn
+         (
+               int nLeftRect,
+               int nTopRect,
+               int nRightRect,
+               int nBottomRect,
+               int nWidthEllipse,
+               int nHeightEllipse
+
+         );
+        public Form1()
+        {
+            InitializeComponent();
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 25, 25));
+            pnlMain.Height = btnNEWS.Height;
+            pnlMain.Top = btnNEWS.Top;
+            pnlMain.Left = btnNEWS.Left;
+            btnNEWS.BackColor = Color.FromArgb(95, 158, 160);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnNEWS_Click(object sender, EventArgs e)
+        {
+            pnlMain.Height = btnNEWS.Height;
+            pnlMain.Top = btnNEWS.Top;
+            pnlMain.Left = btnNEWS.Left;
+            btnNEWS.BackColor = Color.FromArgb(95, 158, 160);
+        }
+
+        private void btnCOINS_Click(object sender, EventArgs e)
+        {
+            pnlMain.Height = btnCOINS.Height;
+            pnlMain.Top = btnCOINS.Top;
+            pnlMain.Left = btnNEWS.Left;
+            btnCOINS.BackColor = Color.FromArgb(95, 158, 160);
+            btnNEWS.BackColor = Color.FromArgb(47, 79, 79);
+        }
+
+        private void btnSettings_Click(object sender, EventArgs e)
+        {
+            pnlMain.Height = btnSettings.Height;
+            pnlMain.Top = btnSettings.Top;
+            pnlMain.Left = btnNEWS.Left;
+            btnSettings.BackColor = Color.FromArgb(95, 158, 160);
+            btnNEWS.BackColor = Color.FromArgb(47, 79, 79);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnNEWS_Leave_1(object sender, EventArgs e)
+        {
+            btnNEWS.BackColor = Color.FromArgb(47, 79, 79);
+        }
+
+        private void btnCOINS_Leave_1(object sender, EventArgs e)
+        {
+            btnCOINS.BackColor = Color.FromArgb(47, 79, 79);
+        }
+
+        private void btnSettings_Leave_1(object sender, EventArgs e)
+        {
+            btnSettings.BackColor = Color.FromArgb(47, 79, 79);
+        }
+    }
+}
