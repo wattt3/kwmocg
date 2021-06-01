287af0405ebbe4c7fa2bb885fff3e834a5becfd9
daeuk <chleodnr3@gmail.com>
2021-06-01 20:41:08 +0900

    News Form
    News Form

--- teampro/News.Designer.cs
+++ teampro/News.Designer.cs
@@ -30,17 +30,168 @@ namespace teampro
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.txt_Keyword = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lvw_Result = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.column_Link = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel3 = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("배달의민족 주아", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(37, 32);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(5, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(181, 37);
+            this.label1.Size = new System.Drawing.Size(104, 38);
             this.label1.TabIndex = 1;
-            this.label1.Text = "News Form";
+            this.label1.Text = "News";
+            // 
+            // txt_Keyword
+            // 
+            this.txt_Keyword.Location = new System.Drawing.Point(6, 32);
+            this.txt_Keyword.Name = "txt_Keyword";
+            this.txt_Keyword.Size = new System.Drawing.Size(126, 21);
+            this.txt_Keyword.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_Search);
+            this.groupBox1.Controls.Add(this.lvw_Result);
+            this.groupBox1.Controls.Add(this.txt_Keyword);
+            this.groupBox1.Location = new System.Drawing.Point(6, 50);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(725, 140);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "검색";
+            // 
+            // lvw_Result
+            // 
+            this.lvw_Result.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.column_Link});
+            this.lvw_Result.HideSelection = false;
+            this.lvw_Result.Location = new System.Drawing.Point(138, 20);
+            this.lvw_Result.Name = "lvw_Result";
+            this.lvw_Result.Size = new System.Drawing.Size(575, 114);
+            this.lvw_Result.TabIndex = 3;
+            this.lvw_Result.UseCompatibleStateImageBehavior = false;
+            this.lvw_Result.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Index";
+            this.columnHeader1.Width = 43;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "제목";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 404;
+            // 
+            // column_Link
+            // 
+            this.column_Link.Text = "링크";
+            this.column_Link.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.column_Link.Width = 100;
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Location = new System.Drawing.Point(37, 72);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 23);
+            this.btn_Search.TabIndex = 4;
+            this.btn_Search.Text = "검색";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.linkLabel3);
+            this.groupBox2.Controls.Add(this.linkLabel2);
+            this.groupBox2.Controls.Add(this.lbl3);
+            this.groupBox2.Controls.Add(this.lbl2);
+            this.groupBox2.Controls.Add(this.lbl1);
+            this.groupBox2.Controls.Add(this.linkLabel1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 196);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(725, 220);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "헤드라인";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(10, 59);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(39, 16);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Link";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.Location = new System.Drawing.Point(10, 31);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(44, 13);
+            this.lbl1.TabIndex = 7;
+            this.lbl1.Text = "label2";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.Location = new System.Drawing.Point(10, 94);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(44, 13);
+            this.lbl2.TabIndex = 8;
+            this.lbl2.Text = "label3";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.Location = new System.Drawing.Point(10, 153);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(44, 13);
+            this.lbl3.TabIndex = 9;
+            this.lbl3.Text = "label4";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(10, 122);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(31, 13);
+            this.linkLabel2.TabIndex = 10;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Link";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // linkLabel3
+            // 
+            this.linkLabel3.AutoSize = true;
+            this.linkLabel3.Location = new System.Drawing.Point(10, 183);
+            this.linkLabel3.Name = "linkLabel3";
+            this.linkLabel3.Size = new System.Drawing.Size(31, 13);
+            this.linkLabel3.TabIndex = 11;
+            this.linkLabel3.TabStop = true;
+            this.linkLabel3.Text = "Link";
+            this.linkLabel3.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel3_LinkClicked);
             // 
             // News
             // 
@@ -48,13 +199,19 @@ namespace teampro
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.CadetBlue;
-            this.ClientSize = new System.Drawing.Size(564, 428);
+            this.ClientSize = new System.Drawing.Size(744, 429);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "News";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "News";
             this.Load += new System.EventHandler(this.News_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -63,5 +220,19 @@ namespace teampro
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txt_Keyword;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.ListView lvw_Result;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader column_Link;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel3;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl1;
     }
 }
--- teampro/News.cs
+++ teampro/News.cs
@@ -9,6 +9,10 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
@@ -19,26 +23,100 @@ namespace teampro
     {
         public Form1 mainForm;
 
+        const string _apiUrl = "https://openapi.naver.com/v1/search/news.json";
+        const string _clientId = "An56e7PSVEZf_WqoRy0Q";
+        const string _clientSecret = "b_2M25JUw7";
+
         protected ChromeDriverService _driverService = null;
         protected ChromeOptions _options = null;
         protected ChromeDriver _driver = null;
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string results = getResults();
+                results = results.Replace("<b>", "");
+                results = results.Replace("</b>", "");
+                results = results.Replace("&lt;", "<");
+                results = results.Replace("&gt;", ">");
+
+                var parseJson = JObject.Parse(results);
+                var countsOfDisplay = Convert.ToInt32(parseJson["display"]);
+                var countsOfResults = Convert.ToInt32(parseJson["total"]);
+
+                lvw_Result.Items.Clear();
+                for (int i = 0; i < countsOfDisplay; i++)
+                {
+                    ListViewItem item = new ListViewItem((i + 1).ToString());
+
+                    var title = parseJson["items"][i]["title"].ToString();
+                    title = title.Replace("&quot;", "\"");
+
+                    var description = parseJson["items"][i]["description"].ToString();
+                    description = description.Replace("&quot;", "\"");
+
+                    var link = parseJson["items"][i]["link"].ToString();
+
+                    item.SubItems.Add(title);
+                    // item.SubItems.Add(description);
+                    item.SubItems.Add(link);
+
+                    lvw_Result.Items.Add(item);
+                }
+            }
+            catch (Exception exc)
+            {
+                Debug.WriteLine(exc.Message);
+            }
+        }
+
+        string _link1, _link2, _link3;
+
+        private string getResults()
+        {
+            string keyword = txt_Keyword.Text;
+            string display = "5";
+            string sort = "sim";
+
+            string query = string.Format("?query={0}&display={1}sort={2}", keyword, display, sort);
+
+            WebRequest request = WebRequest.Create(_apiUrl + query);
+            request.Headers.Add("X-Naver-Client-Id", _clientId);
+            request.Headers.Add("X-Naver-Client-Secret", _clientSecret);
+
+            string requestResult = "";
+            using (var response = request.GetResponse())
+            {
+                using (Stream dataStream = response.GetResponseStream())
+                {
+                    using (var reader = new StreamReader(dataStream))
+                    {
+                        requestResult = reader.ReadToEnd();
+                    }
+                }
+            }
+            return requestResult;
+        } 
 
         public News()
         {
             InitializeComponent();
 
-            try
-            {
-                _driverService = ChromeDriverService.CreateDefaultService();
-                _driverService.HideCommandPromptWindow = true;
+            _driverService = ChromeDriverService.CreateDefaultService();
+            _driverService.HideCommandPromptWindow = true;
 
-                _options = new ChromeOptions();
-                _options.AddArgument("disable-gpu");
-            }
-            catch (Exception exc)
-            {
-                Trace.WriteLine(exc.Message);
-            }
+            _options = new ChromeOptions();
+            _options.AddArgument("headless");
+            IWebDriver _driver = new ChromeDriver(_driverService, _options);
+            _driver.Url = "https://news.naver.com/main/home.nhn";
+
+            lbl1.Text = _driver.FindElement(By.XPath("//*[@id=\"today_main_news\"]/div[2]/ul/li[1]/div[1]/a")).Text;
+            _link1 = _driver.FindElement(By.XPath("//*[@id=\"today_main_news\"]/div[2]/ul/li[1]/div[1]/a")).GetAttribute("href");
+            lbl2.Text = _driver.FindElement(By.XPath("//*[@id=\"today_main_news\"]/div[2]/ul/li[2]/div[1]/a")).Text;
+            _link2 = _driver.FindElement(By.XPath("//*[@id=\"today_main_news\"]/div[2]/ul/li[2]/div[1]/a")).GetAttribute("href");
+            lbl3.Text = _driver.FindElement(By.XPath("//*[@id=\"today_main_news\"]/div[2]/ul/li[3]/div[1]/a")).Text;
+            _link3 = _driver.FindElement(By.XPath("//*[@id=\"today_main_news\"]/div[2]/ul/li[3]/div[1]/a")).GetAttribute("href");
         }
 
         private void News_Load(object sender, EventArgs e)
@@ -51,6 +129,21 @@ namespace teampro
             {
                 this.BackColor = Color.FromArgb(95, 158, 160);
             }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(_link1);
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(_link2);
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start(_link3);
         }
     }
 }
